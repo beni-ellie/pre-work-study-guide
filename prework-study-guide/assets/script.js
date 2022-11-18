ad2706211abc198def6aa12f3f2fb3ac86c45b8b
Benita Nantume <benitanantume@outlook.com>
2022-11-18 12:24:50 +0000

    <added javascript>

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -15,11 +15,12 @@ function selectTopic() {
   } else if (randomTopic === "Git") {
     console.log("Let's study Git!");
   } else if (randomTopic === "JavaScript") {
-    console.log("Let's study JavaScript!");
+    console.log("Let's study JavaScript");
   } else {
-    console.log('Please try again!');
+    console.log("Please ty aian!");
   }
-}
+ }
+ 
 console.log("Here are the topics we learned through Prework:");
 listTopics();
 console.log("Which topic should we study first?");
